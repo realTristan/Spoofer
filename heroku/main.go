46ebb3a60f6e72ad2142ad73265e9faf86721d03
realTristan <heytristaann@gmail.com>
2022-08-10 21:11:19 -0400

    c

---
+++ heroku/main.go
@@ -0,0 +1,179 @@
+package main
+
+// Import the required modules
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+
+	// Fasthttp to host the api
+	"github.com/valyala/fasthttp"
+)
+
+// Fasthttp Request Client
+var RequestClient *fasthttp.Client = &fasthttp.Client{}
+
+// Request Struct
+// The Request structure holds five keys
+//   - Method: string -> the request method
+//   - Url: string -> the request url
+//   - Body: []byte -> the request body
+//   - Headers: map[string]string -> the request headers
+//   - SkipBody: bool -> whether to skip response body for 'HEAD' request
+type Request struct {
+	Method   string
+	Url      string
+	Body     []byte
+	Headers  map[string]string
+	SkipBody bool
+}
+
+// Base64 Decode String
+func Base64Decode(b []byte) string {
+	var decoded, _ = base64.StdEncoding.DecodeString(string(b))
+	return string(decoded)
+}
+
+// The GetHeaderMap() function will get the incoming request headers
+//
+//	then append them to the headers map to be used for the outgoing
+//	request
+func GetHeaderMap(ctx *fasthttp.RequestCtx) map[string]string {
+	// Define a headers map that will hold the keys and values
+	var headers map[string]string = map[string]string{}
+
+	// Iterate through the request headers and add them to the headers map
+	ctx.Request.Header.VisitAll(func(key []byte, value []byte) {
+		headers[string(key)] = string(value)
+	})
+
+	// Return the headers map for later use
+	return headers
+}
+
+// The SetRequest() function will create a new fasthttp request object
+// then set it's url and method using the Request struct
+//
+// After the method and url have been set, it will then set the request headers
+// from the Request struct (previously from the GetHeaderMap() function)
+func SetRequest(req Request) *fasthttp.Request {
+	// Create new request object
+	var request *fasthttp.Request = fasthttp.AcquireRequest()
+
+	// Set request url and method
+	request.SetRequestURI(req.Url)
+	request.Header.SetMethod(req.Method)
+
+	// Set Request Headers
+	for k, v := range req.Headers {
+		request.Header.Set(k, v)
+	}
+	return request
+}
+
+// The SetResponse() function will set the response object
+// for the outgoing request
+//
+// Using the Request struct it will enable / disable the response body
+// through the resp.SkipBody fasthttp function
+func SetResponse(req Request) *fasthttp.Response {
+	// Create a new fasthttp response object
+	var resp *fasthttp.Response = fasthttp.AcquireResponse()
+
+	// Whether to skip the response body if 'HEAD' request
+	resp.SkipBody = req.SkipBody
+
+	// Return response object
+	return resp
+}
+
+// The SendHttpRequest() function will send the outgoing http request
+// to the Request struct url
+//
+// If the Request struct method is POST or PUT then it will set the
+// request body using the defined Request struct body
+//
+// After the method and body have been set, using the RequestClient
+// variable it will send the http request and return the response and the error
+func SendHttpRequest(req Request) (*fasthttp.Response, error) {
+	// Define both the fasthttp request and response objects
+	var (
+		request  *fasthttp.Request  = SetRequest(req)
+		response *fasthttp.Response = SetResponse(req)
+	)
+	// Release the request once no longer needed
+	defer fasthttp.ReleaseRequest(request)
+
+	// Add request body if method is POST or PUT
+	if req.Method == "POST" || req.Method == "PUT" {
+		request.SetBody(req.Body)
+	}
+
+	// Send the http request and set the err variables for return
+	var err error = RequestClient.Do(request, response)
+
+	// Create Response Object
+	return response, err
+}
+
+// The HandleSentRequest() function will set the localhost api
+// response headers, body and status code
+func HandleSentRequest(ctx *fasthttp.RequestCtx, resp *fasthttp.Response, err error) {
+	// Define headers map
+	var headers map[string]string = map[string]string{}
+
+	// Set the response status code
+	ctx.SetStatusCode(resp.StatusCode())
+
+	// Set the response body
+	fmt.Fprint(ctx, string(resp.Body()))
+
+	// Iterate through the request headers and add them to the headers map
+	resp.Header.VisitAll(func(key []byte, value []byte) {
+		headers[string(key)] = string(value)
+	})
+
+	// Set the response headers
+	for k, v := range headers {
+		ctx.Response.Header.Set(k, v)
+	}
+}
+
+// The HandleResponse() function will get the incoming request method
+// and Base64Encode the url param
+//
+// It will then create a new Request struct object with the url, method, body,
+// whether to skip the body, and the incoming request headers
+//
+// Once the Request struct object has been created, it will send the http request
+// and handle the response using a goroutine and the HandleSentRequest() function
+func HandleResponse(ctx *fasthttp.RequestCtx) {
+	var (
+		// Get the request method
+		method string = string(ctx.Request.Header.Method())
+
+		// Get the decoded url to send http request to
+		url string = Base64Decode(ctx.QueryArgs().Peek("url"))
+
+		// Create a new Request struct object
+		req Request = Request{
+			Url:      url,
+			Method:   method,
+			Body:     ctx.Request.Body(),
+			SkipBody: method == "HEAD",
+			Headers:  GetHeaderMap(ctx),
+		}
+
+		// Get the sent request response and error
+		resp, err = SendHttpRequest(req)
+	)
+
+	// Handle the sent http request
+	go HandleSentRequest(ctx, resp, err)
+}
+
+// Main function
+func main() {
+	var port string = os.Getenv("PORT")
+	fasthttp.ListenAndServe(fmt.Sprintf(":%s", port), HandleResponse)
+}
